4611b844ea9c451531b3803325fb2d5e8ce4b1eb
jameshagans <jamesy.h95@gmail.com>
2023-04-07 01:20:11 +0000

    Update: refactor addProperty function to insert form data nto database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -145,7 +145,7 @@ const getAllProperties = function(options, limit = 10) {
     ORDER BY cost_per_night
     LIMIT $${queryParams.length};
   `;
-  
+
   // 5
   console.log(queryString, queryParams);
 
@@ -159,11 +159,18 @@ const getAllProperties = function(options, limit = 10) {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
+
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  console.log('property: ', property);
+  //console.log('length = ', Object.keys(property).length)
+  return pool.query(`INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, province, city, country, street, post_code)  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`, [property.title, property.description, property.owner_id, property.cover_photo_url, property.thumbnail_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.province, property.city, property.country, property.street, property.post_code])
+    .then((res) => {
+      console.log('user added to databse', res.rows[0]);
+      return res.rows[0];
+    }).catch((error) => {
+      console.log('There was an error: ', error);
+    });
 };
 
 module.exports = {
@@ -174,3 +181,17 @@ module.exports = {
   getAllProperties,
   addProperty,
 };
+
+//testing purpose - think about using arrays to math (Object.keys - Object.values - destructure?)
+// const addProperty = function(property) {
+//   console.log('property: ', property);
+//   console.log('length = ', Object.keys(property).length)
+//   return pool.query(`INSERT INTO properties (
+//     title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, province, city, country, street, post_code)  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`, [property.title, property.descrtption, property.owner_id, property.cover_photo_url, property.thumbnail_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.active, property.province, property.city, property.country, property.street, property.post_code ])
+//     .then((res) => {
+//       console.log('user added to databse', res.rows[0]);
+//       return res.rows[0];
+//     }).catch((error) => {
+//       console.log('There was an error: ', error);
+//     });
+// };
